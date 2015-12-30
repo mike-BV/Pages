d45ac95c4acb91c949f94052a15eacbc64da6677
mike-BV <karasists@gmail.com>
2015-12-30 16:18:59 +0800

    change to iframe

--- javascript/main.js
+++
@@ -1,9 +0,0 @@
-$.get('json_cross_domain.php', function(data){
-    var respondcompile = jQuery.parseJSON(data);
-    var cp = respondcompile.fix;
-    for (var index = 0; index < cp.length; index++) {
-        var link = cp[index].icon;
-        var alt = cp[index].title;
-        document.getElementById('imgage').innerHTML +='<img src="'+link+'"'+'alt="'+alt+'"/>';
-    }
-});
--- json_cross_domain.php
+++
@@ -1,6 +0,0 @@
-<?php
-$url='http://www.bilibili.com/index/index-icon.json';
-$response = file_get_contents($url);
-$result = gzdecode($response);
-echo $result;
-?>
